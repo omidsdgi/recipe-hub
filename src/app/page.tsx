839f1d7ea88f3702a2aa7e6acdb9ed74cf2b95ea
omidsdgi <omid69sdgi@gmail.com>
2026-07-10 18:34:35 +0330

    refactor: use Logo component and update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,13 @@
-import Image from "next/image";
+import {Logo} from "@/components";
 
 export default function Home() {
     return (
         <div className="flex items-center justify-center bg-gradient-to-br from-[var(--color-gradient-from)] to-[var(--color-gradient-to)]">
         <div className="my-[4vw] w-full max-w-[120rem] min-h-[117rem] overflow-hidden rounded-[9px] bg-surface shadow-card">
             <header
-                className="
-          flex
-          h-[10rem]
-          items-center
-          gap-8
-          bg-background
-          px-8
-        "
-            >
-                <Image
-                    src="/favicon.png"
-                    alt="Recipe Hub logo"
-                    width={40}
-                    height={40}
-                />
-
-                <h1 className="text-3xl font-bold">
-                    Recipe Hub
-                </h1>
+                className=" flex h-[10rem] w-full items-center justify-between bg-background px-8 ">
+                <Logo/>
+                <h1>Omid</h1>
             </header>
 
             <main
@@ -33,11 +17,11 @@ export default function Home() {
           grid-cols-[1fr_2fr]
         "
             >
-                <section className="bg-surface p-8">
+                <section className="bg-surface p-8 text-3xl">
                     left
                 </section>
 
-                <section className="bg-background p-8">
+                <section className="bg-background p-8 text-3xl">
                     right
                 </section>
             </main>
