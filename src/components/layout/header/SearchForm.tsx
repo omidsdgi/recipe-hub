47a90edd0939add685489412349a9e31685dc815
omidsdgi <omid69sdgi@gmail.com>
2026-07-11 10:48:44 +0330

    fix: update responsive styles

--- src/components/layout/header/SearchForm.tsx
+++ src/components/layout/header/SearchForm.tsx
@@ -4,11 +4,11 @@ import {SearchIcon} from "lucide-react";
 
 const SearchForm = () => {
     return (
-        <form className="flex h-22 w-full max-w-4xl text-3xl items-center rounded-full bg-surface shadow-card " >
+        <form className="flex lg:h-28 w-full lg:max-w-2xl text-xl items-center rounded-full bg-surface shadow-card " >
             <Input
                 type="search"
                 placeholder="Search over 1,000,000 recipes..."
-                className='flex-1 border-0 bg-transparent px-12 font-normal text-foreground placeholder:text-placeholder shadow-none focus-visible:ring-0 focus-visible:ring-offset-0'
+                className='flex-1 border-0 bg-transparent px-12 mb-4 font-normal text-foreground placeholder:text-placeholder shadow-none focus-visible:ring-0 focus-visible:ring-offset-0'
             />
             <Button
             type="submit"
