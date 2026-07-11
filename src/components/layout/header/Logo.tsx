4c1aa402d2a1b5910b5f0bedef9992c9e8371acf
omidsdgi <omid69sdgi@gmail.com>
2026-07-11 10:47:15 +0330

    fix: update max width

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logo() {
     return (
-        <div className="flex items-center justify-between gap-6 ">
+        <div className="flex items-center justify-between max-w-2xs gap-6 mt-4 lg:mt-0 ">
         <Image
             src="/favicon.png"
             alt="Recipe Hub logo"
