7047a54da7862bb1ed7fe577bf20515e650e7cd2
omidsdgi <omid69sdgi@gmail.com>
2026-07-12 21:27:40 +0330

    refactor(theme): rename gradient CSS variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,22 @@ import {SearchForm} from "@/components/layout/header";
 export default function Home() {
     return (
         <div
-            className="flex items-center justify-center bg-gradient-to-br from-[var(--color-gradient-from)] to-[var(--color-gradient-to)]">
+            className="flex items-center justify-center bg-gradient-to-br from-[var(--gradient-from)] to-[var(--gradient-to)]">
                 <div
                     className="my-[4vw] sm:my-6 lg:my-[4vw] w-full max-w-[140rem] lg:min-h-[117rem] overflow-hidden rounded-none sm:roundede:lg lg:rounded-xl bg-surface shadow-card">
                     <header
-                        className=" flex h-[10rem] w-full items-center justify-between bg-background px-8 ">
+                        className=" flex flex-col lg:flex-row gap-6 lg:h-40 h-70 w-full lg:items-center lg:justify-between bg-muted px-6 lg:px-8 ">
                         <Logo/>
                         <SearchForm/>
                         <h1>Omid</h1>
                     </header>
                             <main
                                 className=" grid min-h-[107rem] grid-cols-[1fr_2fr] " >
-                                <section className="bg-surface p-8 text-3xl">
+                                <section className="bg-card p-8 text-3xl">
                                     left
                                 </section>
 
-                                <section className="bg-background p-8 text-3xl">
+                                <section className=" bg-muted p-8 text-3xl">
                                     right
                                 </section>
                             </main>
