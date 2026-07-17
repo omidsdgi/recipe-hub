3368c315be4b88dbf8b40070b9b2bd6c697f7f38
omidsdgi <omid69sdgi@gmail.com>
2026-07-17 11:08:45 +0330

    feat: create DarkModeContext provider

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,27 @@
+import {createContext, ReactNode, useContext} from "react";
+import {Theme} from "@/lib/ThemeCore";
+import {useTheme} from "@/hooks/useTheme";
+
+interface DarkModeContextProps {
+    theme: Theme;
+    isDarkMode: boolean;
+    toggleTheme: () => void;
+}
+
+const DarkModeContext = createContext<DarkModeContextProps | undefined>(undefined)
+
+export function DarkModeProvider({ children }:{ children: ReactNode }) {
+    const value = useTheme()
+
+    return (
+        <DarkModeContext.Provider value={value}>
+            {children}
+        </DarkModeContext.Provider>
+    )
+}
+
+export function useDarkMode():DarkModeContextProps {
+    const context=useContext(DarkModeContext);
+    if (context === undefined) throw new Error('DarkModeContext was used outside of DarkModeProvider');
+    return context;
+}
