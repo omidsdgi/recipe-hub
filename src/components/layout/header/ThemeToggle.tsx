b4a750cf90a9622ada008148c5853270d99ba6ff
omidsdgi <omid69sdgi@gmail.com>
2026-07-17 11:11:59 +0330

    feat: implement ThemeToggle component

--- src/components/layout/header/ThemeToggle.tsx
+++ src/components/layout/header/ThemeToggle.tsx
@@ -1,5 +1,18 @@
+import {Button} from "@/components/ui/button";
+import {Moon, Sun} from "lucide-react";
+import {useDarkMode} from "@/context/DarkModeContext";
 
-export function useTheme() {
-    return
+export default function ThemeToggle() {
+    const {isDarkMode,toggleTheme}=useDarkMode()
+    return (
+        <Button onClick={toggleTheme}
+            variant="ghost"
+            size="icon"
+            className='h-16 w-16 rounded-full'
+            aria-label='Toggle Theme'
+        >
+            {isDarkMode ? <Sun className='size-8'/> : <Moon className='size-8'/> }
+        </Button>
+    )
 }
 
