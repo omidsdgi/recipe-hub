595e43998418cf166c95d36f9235345277fe21eb
omidsdgi <omid69sdgi@gmail.com>
2026-07-25 19:32:24 +0330

    feat: implement useRecipe hook

---
+++ src/hooks/useRecipe.ts
@@ -0,0 +1,26 @@
+import {Recipe} from "@/types/Recipe";
+import {useEffect, useState} from "react";
+import {getRecipe} from "@/services/RecipeService";
+
+export function useRecipe(id: string) {
+    const [recipe, setRecipe] = useState<Recipe | null>(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        async function loadRecipe() {
+            try{
+                setIsLoading(true);
+                const data=await getRecipe(id)
+                setRecipe(data)
+            }catch(err){
+                if (err instanceof Error) {
+                    setError('Something went wrong')
+                }
+            }finally {
+                setIsLoading(false)
+            }
+        }
+      void  loadRecipe()
+    },[id])
+}
