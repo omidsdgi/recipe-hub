123de278dc74aad988cbd0205e29dc0955dd4a86
omidsdgi <omid69sdgi@gmail.com>
2026-07-26 22:16:33 +0330

    refactor(page): replace layout sections with  Sidebar and Recipe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 
 import {Header} from "@/components/layout";
-import {getRecipe} from "@/services/RecipeService";
-import {Recipe} from "@/recipe";
+import {Sidebar} from "@/sidebar";
+import {Recipe} from "@/components";
 
 
 export default async function Home() {
-    const recipe =await getRecipe("664c8f193e7aa067e94e8823")
-
     return (
         <div
             className="flex items-center justify-center bg-gradient-to-br from-[var(--gradient-from)] to-[var(--gradient-to)]">
@@ -15,15 +13,9 @@ export default async function Home() {
                 <Header/>
                 <main
                     className=" grid min-h-[107rem] grid-cols-[1fr_2fr] " >
-                    <section className="bg-card p-8 text-3xl">
-                        left
-                    </section>
-
-                    <section className=" bg-muted p-8 text-3xl">
-                        <Recipe recipe={recipe} />
-                    </section>
+                   <Sidebar/>
+                    <Recipe/>
                 </main>
-                {/*<RecipeHero image='/Maryam.jpg' title='mehraneh'/>*/}
             </div>
         </div>
     );
