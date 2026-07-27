debfeb7cc9c9c18b6c689a157ea3173d1d91d6e9
omidsdgi <omid69sdgi@gmail.com>
2026-07-27 21:01:06 +0330

    refactor(recipe-hero): add gradient styling to title span

--- src/components/recipe/RecipeHero.tsx
+++ src/components/recipe/RecipeHero.tsx
@@ -7,7 +7,7 @@ interface RecipeHeroProps {
 export default function RecipeHero({recipe}:RecipeHeroProps) {
     const {title, image}=recipe;
     return (
-        <figure className="relative h-[32rem] overflow-hidden">
+        <figure className="relative h-[32rem] ">
             <img
                 src={image}
                 alt={title}
@@ -15,9 +15,14 @@ export default function RecipeHero({recipe}:RecipeHeroProps) {
             />
             <div className="absolute inset-0  bg-gradient-to-br from-gradient-from to-gradient-to opacity-60"/>
 
-            <h1 className="absolute bottom-6 left-1/2 -translate-x-1/2 rotate-[-3deg] bg-gradient-to-r from-primary to-primary-hover px-10 py-4 text-center text-4xl font-semibold uppercase tracking-wide text-white shadow-xl"
+            <h1
+                className="absolute bottom-0 left-1/2 z-10 w-1/2 -translate-x-1/2 translate-y-1/5 -skew-y-6 text-center text-5xl font-bold uppercase leading-[1.95] text-white max-md:w-[70%]"
             >
-                {recipe.title}
+  <span
+      className=" box-decoration-clone bg-gradient-to-br from-gradient-from to-gradient-to px-8 py-5"
+  >
+    {recipe.title}
+  </span>
             </h1>
         </figure>
     );
