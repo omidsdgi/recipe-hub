ab0a1dc459b2307a0a7e7417230770591a83a9b5
omidsdgi <omid69sdgi@gmail.com>
2026-07-28 19:34:01 +0330

    feat(Recipe-Details): implement RecipDetails UI

--- src/components/recipe/RecipeDetails.tsx
+++ src/components/recipe/RecipeDetails.tsx
@@ -1,7 +1,57 @@
-const RecipeDetails = () => {
+import {RecipeType} from "@/types/Recipe";
+import {Bookmark, Clock, MinusCircle, PlusCircle, User} from "lucide-react";
+import {Button} from "@/components/ui/button";
+
+interface RecipeDetailsProps {
+    recipe: RecipeType;
+}
+
+const RecipeDetails = ({recipe}:RecipeDetailsProps) => {
+    const{cookingTime, servings}=recipe
     return (
-        <section>
-            RecipeDetails
+
+        <section className="flex items-center justify-evenly border-b border-border mt-20 px-10 py-8 ">
+            {/*left*/}
+            <div className="flex items-center gap-22">
+                {/*cooking time*/}
+                <div className="flex items-center gap-4">
+                    <Clock size={26} className="text-primary" />
+                    <div className="flex items-baseline gap-2">
+                        <span className="text-2xl font-semibold ">
+                            {cookingTime}
+                        </span>
+                        <span className="text-2xl font-semibold uppercase tracking-wide ">
+                            minutes
+                        </span>
+                    </div>
+                </div>
+                    {/*servings*/}
+                    <div className="flex items-center gap-3">
+                        <User size={24} className="text-primary" />
+                        <div className="flex items-baseline gap-2 ">
+                            <span className="text-2xl font-semibold">22</span>
+                            <span className="text-2xl font-semibold ">
+                                {servings}
+                            </span>
+                        </div>
+                        <div className="ml-4 flex items-center gap-2">
+                            <Button size="icon" variant='ghost' className='rounded-full' arial-label="Decrease servings" >
+                                <MinusCircle className='size-8 text-primary' />
+                            </Button>
+                            <Button size="icon" variant='ghost' className='rounded-full' arial-label="Increase servings" >
+                                <PlusCircle className='size-8 text-primary' />
+                            </Button>
+                        </div>
+                    </div>
+            </div>
+                {/*right*/}
+                <div className="flex items-center gap-4">
+                    <User size='24' className="text-primary" />
+                    <Button size='icon-5xl' className='rounded-full bg-gradient-to-br from-graient-from to-gradient-to ' arial-label="Bookmark recipe" >
+                        <Bookmark className='size-8 fill-background ' />
+                    </Button>
+
+            </div>
         </section>
     );
 };
