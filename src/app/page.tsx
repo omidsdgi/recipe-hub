b29467e6d932c698a36c910bcfbfba19ee6a882b
omidsdgi <omid69sdgi@gmail.com>
2026-07-28 19:35:54 +0330

    refactor(page): update page width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
         <div
             className="flex items-center justify-center bg-gradient-to-br from-gradient-from to-gradient-to">
             <div
-                className="my-[4vw] sm:my-6 lg:my-[4vw] w-full max-w-[140rem] lg:min-h-[117rem] overflow-hidden rounded-none sm:roundede:lg lg:rounded-xl bg-surface shadow-card">
+                className="my-[4vw] sm:my-6 lg:my-[4vw] w-full max-w-[120rem] lg:min-h-[117rem] overflow-hidden rounded-none sm:roundede:lg lg:rounded-xl bg-surface shadow-card">
                 <Header/>
                 <main
                     className=" grid min-h-[107rem] grid-cols-[1fr_2fr] " >
