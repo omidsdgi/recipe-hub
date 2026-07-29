90da9872b239217178338188a6df7011adbbb399
omidsdgi <omid69sdgi@gmail.com>
2026-07-29 20:34:10 +0330

    feat(Ingredient-Item): implement component structure

--- src/components/recipe/IngredientItem.tsx
+++ src/components/recipe/IngredientItem.tsx
@@ -1,8 +1,24 @@
-const IngredientItem = () => {
+import {IngredientType} from "@/types/Recipe";
+import {Check} from "lucide-react";
+
+interface IngredientItemProps {
+    ingredient: IngredientType
+}
+
+const IngredientItem = ({ingredient}: IngredientItemProps) => {
+    const {quantity, unit, description} = ingredient;
     return (
-        <section>
-            IngredientItem
-        </section>
+        <li>
+            <Check />
+            <span> </span>
+            <p >{quantity}
+                {unit && (
+                    <span >{unit}
+                        {unit}
+                </span>)}
+                {description}
+            </p>
+        </li>
     );
 };
 
