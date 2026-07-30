d92b1250a60316b109d5b7f5a0e914d320edd27c
omidsdgi <omid69sdgi@gmail.com>
2026-07-30 09:48:05 +0330

    refactor(styles): update IngredientList background color

--- src/components/recipe/IngredientList.tsx
+++ src/components/recipe/IngredientList.tsx
@@ -6,7 +6,7 @@ interface IngredientsProps {
 }
 const IngredientList = ({ingredients}:IngredientsProps) => {
     return (
-        <section className=" px-[8rem] py-[3rem] ">
+        <section className=" px-[8rem] py-[3rem] bg-section ">
             <h2 className='mb-10 text-3xl font-bold text-primary uppercase tracking-wide text-center'>
                 Recipe Ingredients
             </h2>
