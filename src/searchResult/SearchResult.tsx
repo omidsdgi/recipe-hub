06ca7d483036e5960139a79c24db12167ef45736
omidsdgi <omid69sdgi@gmail.com>
2026-08-21 19:27:01 +0330

    feat(search-result): show Spinner while loading  results

--- src/searchResult/SearchResult.tsx
+++ src/searchResult/SearchResult.tsx
@@ -1,7 +1,10 @@
+import {Spinner} from "@/components";
+
 const SearchResult = () => {
     return (
         <section className="bg-card p-8 text-3xl">
             <h1>searchResult</h1>
+            <Spinner/>
         </section>
     );
 };
