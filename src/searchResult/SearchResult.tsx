0288cb7d78c6ba6b4121353b08e353bbd268ad3d
omidsdgi <omid69sdgi@gmail.com>
2026-09-02 19:09:53 +0330

    feat(view): add recipe links with hash IDS

--- src/searchResult/SearchResult.tsx
+++ src/searchResult/SearchResult.tsx
@@ -3,7 +3,18 @@ import {Spinner} from "@/components";
 const SearchResult = () => {
     return (
         <section className="bg-card p-8 text-3xl bg-section">
-            <h1>searchResult</h1>
+
+            <ul>
+            <a href="#5ed6604591c37cdc054bc886">
+                <div>
+                <h4>Spicy Chicken and Pepper Jack Pizza</h4>
+                </div></a>
+            <a href="#664c8f193e7aa067e94e8297">
+                <div>
+                   <h4>Double Crust Stuffed Pizza</h4>
+                </div>
+            </a>
+            </ul>
             <Spinner/>
         </section>
     );
