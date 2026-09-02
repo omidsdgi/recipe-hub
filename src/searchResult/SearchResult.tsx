1e04636a85dc28d45701a82ce28bd3b66b198529
omidsdgi <omid69sdgi@gmail.com>
2026-09-02 13:46:49 +0330

    feat(Search-result): show dynamic section background color

--- src/searchResult/SearchResult.tsx
+++ src/searchResult/SearchResult.tsx
@@ -2,7 +2,7 @@ import {Spinner} from "@/components";
 
 const SearchResult = () => {
     return (
-        <section className="bg-card p-8 text-3xl">
+        <section className="bg-card p-8 text-3xl bg-section">
             <h1>searchResult</h1>
             <Spinner/>
         </section>
