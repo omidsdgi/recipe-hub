15d6315ffbe0ec894953dc61f63f7a18d331148b
omidsdgi <omid69sdgi@gmail.com>
2026-09-02 19:41:56 +0330

    feat(hash): handle recipe hash and initial load

---
+++ src/searchResult/Hash.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import {Recipe} from "@/components";
+import {SearchResult} from "@/searchResult/index";
+
+const Hash = () => {
+    const [id, setId] = useState('')
+    useEffect(() => {
+        const useRecipe=()=>{
+            const id= window.location.hash.split('#')[1]
+            setId(id)
+        }
+        ['hashchange','load'].forEach(ev=>
+        window.addEventListener(ev,useRecipe)
+        )
+        return () => ['hashchange', 'load'].forEach(ev=>
+            window.removeEventListener('hashchange', useRecipe)
+        )
+    },[])
+
+    return (
+        <>
+            <SearchResult/>
+                {id && <Recipe id={id} />}
+        </>
+    );
+};
+
+export default Hash;
