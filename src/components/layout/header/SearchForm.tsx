a2cacde4ff6bbd04d809820b6ee098fbb28accac
omidsdgi <omid69sdgi@gmail.com>
2026-09-12 17:54:59 +0330

    fear(search-form): handle recipe search submission

--- src/components/layout/header/SearchForm.tsx
+++ src/components/layout/header/SearchForm.tsx
@@ -1,12 +1,31 @@
+'use client'
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {SearchIcon} from "lucide-react";
+import React, {useState} from "react";
 
-const SearchForm = () => {
+interface SearchFormProps {
+    onSearch: (query: string) => void;
+    isLoading?:boolean;
+}
+
+const SearchForm = ({onSearch, isLoading}:SearchFormProps) => {
+    const [query, setQuery] = useState("")
+
+    function handleSubmit(e:React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const trimmedQuery=query.trim()
+          if(!trimmedQuery) return
+        onSearch(trimmedQuery)
+    }
     return (
-        <form className="flex lg:h-24 w-full lg:max-w-3xl text-xl lg:text-2xl lg:font-semibold items-center rounded-full bg-card shadow-card " >
+        <form
+            onSubmit={handleSubmit}
+            className="flex lg:h-24 w-full lg:max-w-3xl text-xl lg:text-2xl lg:font-semibold items-center rounded-full bg-card shadow-card " >
             <Input
                 type="search"
+                value={query}
+                onChange={(e)=>setQuery(e.target.value)}
                 placeholder="Search over 1,000,000 recipes..."
                 className='flex-1 border-0 bg-transparent px-12 mb-4 font-normal text-foreground placeholder:text-placeholder shadow-none focus-visible:ring-0 focus-visible:ring-offset-0'
             />
@@ -14,6 +33,7 @@ const SearchForm = () => {
             type="submit"
            variant="recipe"
             size='recipe'
+            disabled={isLoading}
             >
                 <SearchIcon className='size-10 mr-6'/>
                 Search
