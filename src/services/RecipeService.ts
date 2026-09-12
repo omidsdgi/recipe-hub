48dd2ecda2ddbeaa9a6186bf0ac7770067e3c038
omidsdgi <omid69sdgi@gmail.com>
2026-09-12 14:08:37 +0330

    feat(recipe-services): implement recipe search result

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -2,9 +2,16 @@ import {RecipeType} from "@/types/Recipe";
 import {API_URL, TIMEOUT_SEC} from "@/config/constants";
 import {timeout} from "@/helpers/timeout";
 
+export  interface SearchResultType{
+    id: string;
+    title: string;
+    publisher: string;
+    image: string;
+}
+
 export async function getRecipe(id: string): Promise<RecipeType> {
     const res = await Promise.race([
-        fetch(`${API_URL}/${id}`),
+        fetch(`${API_URL}${id}`),
         timeout(TIMEOUT_SEC)
     ])
     const data = await res.json();
@@ -26,3 +33,30 @@ export async function getRecipe(id: string): Promise<RecipeType> {
     }
 }
 
+export async function loadSearchResults  (
+    query:string
+):Promise<SearchResultType[]>  {
+    const res = await Promise.race([
+        fetch(`${API_URL}?search=${query}`),
+        timeout(TIMEOUT_SEC)
+    ])
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message}${res.status}`);
+    console.log(data.data.recipes);
+
+    return data.data.recipes.map(
+        (recipe:{
+            id:string,
+            title:string,
+            publisher:string,
+            image_url:string,
+    }) => ({
+            id:recipe.id,
+            title:recipe.title,
+            publisher:recipe.publisher,
+            image:recipe.image_url,
+    })
+    )
+}
