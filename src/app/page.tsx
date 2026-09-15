45c81e8c561fedf43475fc2245614274773acc09
omidsdgi <omid69sdgi@gmail.com>
2026-09-15 13:32:19 +0330

    refactor (page): centralize recipe search state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
+import {RecipeHub} from "@/components";
 
-import {Header} from "@/components/layout";
-import {RecipePage} from "@/components";
-
-export default async function Home() {
+export default function Home() {
     return (
-        <div
-            className="flex items-center justify-center bg-gradient-to-br from-gradient-from to-gradient-to">
-            <div
-                className="my-[4vw] sm:my-6 lg:my-[4vw] w-full max-w-[120rem] lg:min-h-[117rem] overflow-hidden rounded-none sm:roundede:lg lg:rounded-xl bg-surface shadow-card">
-                <Header/>
-                <main
-                    className=" grid min-h-[107rem] grid-cols-[1fr_2fr] " >
-                 <RecipePage/>
-                </main>
+        <div className="flex items-center justify-center bg-gradient-to-br from-gradient-from to-gradient-to">
+            <div className="my-[4vw] w-full max-w-[120rem] overflow-hidden rounded-none bg-surface shadow-card sm:my-6 sm:rounded-none lg:my-[4vw] lg:min-h-[117rem] lg:rounded-xl">
+                <RecipeHub/>
             </div>
         </div>
     );
